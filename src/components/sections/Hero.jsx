29a1588bc514489eb9ed8cb86fbfe517f6a321d5
RagaRidzki <ragaridzkyy@gmail.com>
2025-06-04 09:45:17 +0700

    fix(hero.jsx): mengubah nama hero.svg

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import HeroImage from "../../assets/images/Hero.svg";
+import HeroImage from "../../assets/images/hero.svg";
 
 const Hero = () => {
   return (
